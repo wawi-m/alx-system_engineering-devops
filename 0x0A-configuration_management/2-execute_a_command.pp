b4823eb39081ae55ac30b69a1692af4ed4da890d
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-02 13:51:53 +0000

    Update file 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,11 @@
+#create killmenow
+
+file { '/usr/local/bin/killmenow':
+  ensure  => present,
+  mode    => '755',  # Ensure executable permission
+  content => '#!/bin/bash\nwhile true; do sleep 2; done\n',
+}
+
 # kill a process
 
 exec{ 'kill_killmenow_process':
