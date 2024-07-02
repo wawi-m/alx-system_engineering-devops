374dfbb503fde9c26391cd436914c04429a97030
wawi <wawigladys@gmail.com>
2024-07-02 06:26:06 -0700

    update file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,15 @@
+# check for python instal
+
+package{ 'python3':
+  ensure => installed,
+}
+
+# check for pip3 instal
+
+package{ 'python3-pip':
+  ensure => installed,
+}
+
 # Install Flask v 2.1.0 using pip3
 
 package { 'flask':
