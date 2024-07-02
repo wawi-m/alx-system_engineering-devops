fec6c1e2c3dfa7aacf4061b423639821329d9c78
wawi <wawigladys@gmail.com>
2024-07-02 06:47:28 -0700

    update file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -15,4 +15,13 @@ package{ 'python3-pip':
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install werkzeug using pip3
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
