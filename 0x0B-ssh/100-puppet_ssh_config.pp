d60be8eaea533e36bdb6342a7d0f31072dfbb1b1
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-12 07:31:59 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,3 +7,11 @@ PasswordAuthentication no
 IdentityFile ~/.ssh/school
 ",
 }
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
