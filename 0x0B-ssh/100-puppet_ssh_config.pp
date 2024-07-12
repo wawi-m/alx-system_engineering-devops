141cfb1c9db4f1f7401961721fe9c6cd94d9c6d9
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-12 07:02:21 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,19 @@
-# change conf using puppet
 #!/usr/bin/env bash
+# change conf using puppet
 
 file { '~/.ssh/config':
   ensure => 'present',
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => 'PasswordAuthentication yes',
   replace => 'True',
 }
 
 file_line { 'Declare identity file':
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   match  => 'IdentityFile',
   ensure => 'present'
