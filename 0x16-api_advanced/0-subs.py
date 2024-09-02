cc98d58259b9e3a431118ce15b15588f902e0654
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-02 23:15:32 +0300

    Create 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     Return number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"user-agent": "Brave/1.69"}
+    headers = {"user-agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
