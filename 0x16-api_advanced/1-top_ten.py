7f08646fc14448f8b11f36744ca976c285a00a8c
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-03 00:19:08 +0300

    Create 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
 '''
-Module contains the function top_ten
+    this module contains the function top_ten
 '''
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    '''Returns a subreddit's top ten posts'''
-    headers = {'User-Agent': 'Lizzie'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
-    
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
-        
-        data = response.json()
-        children = data.get('data', {}).get('children', [])
-        
-        if not children:
-            print(None)
-            return
-        
-        for post in children:
-            print(post.get('data', {}).get('title', None))
-            
-    except requests.RequestException:
-        # Catch any network-related errors or bad HTTP responses
-        print(None)
-    except ValueError:
-        # Handle JSON decoding errors
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        top_ten(argv[1])
-    else:
-        print("Usage: ./1-top_ten.py <subreddit>")
+    top_ten(argv[1])
