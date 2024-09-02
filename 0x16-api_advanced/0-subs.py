5a1e105fdb4f3843927fa9043704588a83fb3adc
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-02 23:31:17 +0300

    Create 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Queries the Reddit API and returns the number of subscribers for a given subreddit
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "request"}
+    """Return number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Request"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        return data["data"]["subscribers"]
+    elif response.status_code == 302:
+        return 0
     else:
         return 0
