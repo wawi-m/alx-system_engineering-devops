15bb5591db5fd6cdd57c82158f2ba55de3783c41
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-02 23:55:46 +0300

    Create 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 '''
-    this module contains the function top_ten
+Module contains the function top_ten
 '''
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
+    '''Returns a subreddit's top ten posts'''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                        .format(subreddit), headers=user).json()
