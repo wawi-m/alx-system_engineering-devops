ffe8039f99c03a2f32938186ce99dec7fd6ccd09
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-03 01:15:03 +0300

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change the OS config to login with holberton user 
+# and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
