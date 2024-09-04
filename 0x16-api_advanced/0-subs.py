fb89b32efff86b8612c3f2a5c38a675ac42f060f
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-05 00:06:05 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ def number_of_subscribers(subreddit):
     """
     Return number of a subreddit's subscribers
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return (data['data']['subscribers'])
+        return data.get('data', {}).get('subscribers', 0)
     else:
         return 0
